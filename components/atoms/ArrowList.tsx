16804f335888033251fe859a6b05c1a801563fab
qTeePie <isabel@chaparian.no>
2026-02-23 19:23:43 +0100

    ui(arrow-list): generic roving tabindex list component

--- components/atoms/ArrowList.tsx
+++ components/atoms/ArrowList.tsx
@@ -1,3 +1,57 @@
-export function ArrowList() {
-  return <div>hello</div>
+import { ReactNode } from 'react'
+
+type ArrowListProps<T> = {
+  items: T[]
+  getId: (item: T) => string
+  selectedId: string | null
+  onSelect: (item: T) => void
+  children: (args: { item: T; isSelected: boolean; onSelect: (item: T) => void }) => ReactNode
+  className: string
+}
+
+export function ArrowList<T>({
+  items,
+  getId,
+  selectedId,
+  onSelect,
+  children,
+  className = 'card flex-1 overflow-y-auto no-scrollbar',
+}: ArrowListProps<T>) {
+  return (
+    <ul
+      className={className}
+      tabIndex={0}
+      onKeyDown={e => {
+        if (e.key === 'Home') {
+          e.preventDefault()
+          onSelect(items[0])
+          return
+        }
+
+        if (e.key === 'End') {
+          e.preventDefault()
+          onSelect(items[items.length - 1])
+          return
+        }
+
+        if (e.key !== 'ArrowDown' && e.key !== 'ArrowUp') return
+        e.preventDefault()
+
+        const index = items.findIndex(it => getId(it) === selectedId)
+        if (index === -1) return
+
+        let next = index
+        if (e.key === 'ArrowDown') next = Math.min(index + 1, items.length - 1)
+        if (e.key === 'ArrowUp') next = Math.max(index - 1, 0)
+
+        onSelect(items[next])
+      }}
+    >
+      {items.map(item => {
+        const isSelected = getId(item) === selectedId
+
+        return children({ item, isSelected, onSelect: () => onSelect(item) })
+      })}
+    </ul>
+  )
 }
