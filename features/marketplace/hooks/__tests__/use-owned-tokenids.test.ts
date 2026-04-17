90871f3ce0c39fc128e1e7c836a5c7fc0b2d9246
qTeePie <isabel@chaparian.no>
2026-04-17 23:16:05 +0200

    test(use-owned-tokenids): test fetch on mount

--- features/marketplace/hooks/__tests__/use-owned-tokenids.test.ts
+++ features/marketplace/hooks/__tests__/use-owned-tokenids.test.ts
@@ -1,24 +1,36 @@
-import { describe, it, vi } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import { useOwnedTokenIds } from '../use-owned-tokenids'
-import { renderHook, RenderHookResult } from '@testing-library/react'
+import { renderHook, RenderHookResult, waitFor } from '@testing-library/react'
 import { Hex } from '@/domain/shared/eth'
 
 describe('useOwnedTokenIds', () => {
   type HookProps = Parameters<typeof useOwnedTokenIds>
   type OwnedTokenIdsHook = RenderHookResult<ReturnType<typeof useOwnedTokenIds>, HookProps>
 
-  describe('initial fetch', () => {
-    it('calls readOwnedFn on initial render if valid inputs')
+  describe('mount', () => {
+    it('auto fetches on mount when inputs are valid', async () => {
+      const readMock = vi.fn().mockResolvedValue([])
+
+      renderHook(() => useOwnedTokenIds('0xabc', '0x123', readMock))
+
+      await waitFor(() => expect(readMock).toHaveBeenCalled())
+    })
   })
 
   describe('manual refetch', () => {
+    const getRefetch = (hook: OwnedTokenIdsHook) => hook.result.current.refetch
+
     it.each([
       ['collection', undefined as Hex | undefined, '0xaccount' as Hex],
       ['account', '0xcollection' as Hex, undefined as Hex | undefined],
     ])('does nothing when %s is undefined', async (_, collection, account) => {
       const readMock = vi.fn()
+      const hook = renderHook(() => useOwnedTokenIds(collection, account, readMock))
 
-      renderHook(() => useOwnedTokenIds(collection, account, readMock))
+      const refetch = getRefetch(hook)
+      await refetch()
+
+      expect(readMock).not.toHaveBeenCalled()
     })
   })
 
