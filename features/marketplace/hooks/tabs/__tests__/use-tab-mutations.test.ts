dfd4b41ccc52afb576310290e6a5e663f8a2aa58
qTeePie <isabel@chaparian.no>
2026-04-21 02:57:22 +0200

    test(use-tab-mutations): test suite mergePage + replacePage

--- features/marketplace/hooks/tabs/__tests__/use-tab-mutations.test.ts
+++ features/marketplace/hooks/tabs/__tests__/use-tab-mutations.test.ts
@@ -10,7 +10,7 @@ import { useTabMutations } from '../use-tab-mutations'
 type TabPages = { [K in TabName]: Page<TabResource[K]> }
 
 describe('use-tab-mutations', () => {
-  // the hook input `setState` is of tpe Dispatch<SetStateAction<TabPages>>
+  // the hook input `setState` is of type Dispatch<SetStateAction<TabPages>>
   // that means: a function that accepts either a new state, or a function that takes the old state (prev)
   // and returns a new state.
   // Dispatch: a function that takes one argument and returns nothing
@@ -64,29 +64,29 @@ describe('use-tab-mutations', () => {
   }
 
   describe('addItem', () => {
-    const itemToAdd = { id: '999' } as TabResource['feed']
+    const addedItem = { id: '999' } as TabResource['feed']
 
     it('prepends new item to tab items', () => {
       const getState = renderHookAndAct({
-        run: m => m.addItem('feed', itemToAdd),
+        run: m => m.addItem('feed', addedItem),
         withState: populatedFeed,
       })
 
-      expect(getState().feed.items[0]).toBe(itemToAdd)
+      expect(getState().feed.items[0]).toBe(addedItem)
     })
 
     it('keeps existing tab items', () => {
       const getState = renderHookAndAct({
-        run: m => m.addItem('feed', itemToAdd),
+        run: m => m.addItem('feed', addedItem),
         withState: populatedFeed,
       })
 
-      expect(getState().feed.items).toEqual([itemToAdd, ...populatedFeed.feed.items])
+      expect(getState().feed.items).toEqual([addedItem, ...populatedFeed.feed.items])
     })
 
     it('does not affect other tabs', () => {
       const getState = renderHookAndAct({
-        run: m => m.addItem('feed', itemToAdd),
+        run: m => m.addItem('feed', addedItem),
         withState: { ...populatedFeed, ...populatedSales, ...populatedExplore },
       })
 
@@ -145,5 +145,78 @@ describe('use-tab-mutations', () => {
     })
   })
 
-  describe('mergePage', () => {})
+  describe('mergePage', () => {
+    const newItems = [{ id: 'm_1' }, { id: 'm_2' }, { id: 'm_3' }] as TabResource['feed'][]
+    const newCursor = 'cursor_123'
+
+    it('merges new items into existing page', () => {
+      // smoke test ensures existing state and newItems have no mutual members
+      const sharedIds = newItems.filter(n => populatedFeed.feed.items.some(e => e.id === n.id))
+      expect(sharedIds).toHaveLength(0)
+
+      const getState = renderHookAndAct({
+        run: m => m.mergePage('feed', newItems, newCursor),
+        withState: populatedFeed,
+      })
+
+      expect(getState().feed).toEqual({
+        items: [...populatedFeed.feed.items, ...newItems],
+        cursor: newCursor,
+      })
+    })
+
+    it('sets cursor to new string value', () => {
+      const getState = renderHookAndAct({
+        run: m => m.mergePage('feed', [], newCursor),
+        withState: { feed: { items: [], cursor: null } },
+      })
+
+      expect(getState().feed.cursor).toBe(newCursor)
+    })
+
+    it('sets cursor to new null value', () => {
+      const getState = renderHookAndAct({
+        run: m => m.mergePage('feed', [], null),
+        withState: { feed: { items: [], cursor: 'old' } },
+      })
+
+      expect(getState().feed.cursor).toBe(null)
+    })
+
+    it('does not insert duplicates', () => {
+      const duplicate = populatedFeed.feed.items[0]
+
+      const getState = renderHookAndAct({
+        run: m => m.mergePage('feed', [...newItems, duplicate], newCursor),
+        withState: populatedFeed,
+      })
+
+      expect(getState().feed.items.filter(item => item.id === duplicate.id)).toHaveLength(1)
+    })
+
+    it('does not affect other tabs', () => {
+      const getState = renderHookAndAct({
+        run: m => m.mergePage('feed', newItems, newCursor),
+        withState: { ...populatedFeed, ...populatedSales, ...populatedExplore },
+      })
+
+      expect(getState()).toMatchObject({ ...populatedSales, ...populatedExplore })
+    })
+  })
+
+  describe('replacePage', () => {
+    const newPage = {
+      items: [{ id: 'r_1' }, { id: 'r_2' }, { id: 'r_3' }] as TabResource['feed'][],
+      cursor: 'cursor_123',
+    }
+
+    it('replaces tab page', () => {
+      const getState = renderHookAndAct({
+        run: m => m.replacePage('feed', newPage),
+        withState: populatedFeed,
+      })
+
+      expect(getState().feed).toEqual(newPage)
+    })
+  })
 })
