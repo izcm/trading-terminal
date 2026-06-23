15a5ea0020c74ac5f7cb7a72141ff40ef50b46b2
izcm <isabel@chaparian.no>
2026-06-24 00:38:50 +0200

    use @a2zb/abis + audit packages

--- protocol/config.ts
+++ protocol/config.ts
@@ -1,4 +1,4 @@
-import json from '@a2zb/packages/abis/dmrkt/OrderEngine.json'
+import json from '@a2zb/abis/dmrkt/OrderEngine.json'
 import { Abi } from 'viem'
 
 export const orderbookAbi = json.abi as Abi
