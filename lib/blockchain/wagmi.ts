b7c6f554c98e132dafe98b385d942942635124db
izcm <isabel@chaparian.no>
2026-07-15 00:14:56 +0200

    config: implement special handling of anvil chain in wagmi.ts

--- lib/blockchain/wagmi.ts
+++ lib/blockchain/wagmi.ts
@@ -5,19 +5,33 @@ import { injected } from 'wagmi/connectors'
 
 import supportedChains from '@/chains.json'
 
+// if demo mode -> only include anvil
+const activeChains =
+  process.env.NEXT_PUBLIC_MODE === 'DEMO'
+    ? supportedChains.filter(c => c.chainId === 31337)
+    : supportedChains.filter(c => c.chainId !== 31337)
+
 type ChainExtras = wagmiChains.Chain & { marketplace: Address; weth: Address }
 
-const configuredChains: ChainExtras[] = supportedChains.map(sc => {
+// anvil's marketplace address changes per-deploy (redeployed contracts)
+// to avoid any ambiguity any use of anvil has to set the marketplace address
+// as NEXT_PUBLIC env variable
+if (activeChains.some(sc => sc.chainId === 31337) && !process.env.NEXT_PUBLIC_ANVIL_MARKETPLACE) {
+  throw new Error('NEXT_PUBLIC_ANVIL_MARKETPLACE must be set when anvil is a configured chain')
+}
+
+const configuredChains: ChainExtras[] = activeChains.map(sc => {
+  const isAnvil = sc.chainId === 31337
   return {
     ...Object.values(wagmiChains).find(wc => wc.id === sc.chainId)!,
-    marketplace: sc.marketplace as Address,
+    marketplace: (isAnvil ? process.env.NEXT_PUBLIC_ANVIL_MARKETPLACE! : sc.marketplace) as Address,
     weth: sc.weth as Address,
   }
 })
 
 if (configuredChains.length === 0) throw new Error('no supported chains configured')
 
-const transports = Object.fromEntries(supportedChains.map(sc => [sc.chainId, http(sc.url)]))
+const transports = Object.fromEntries(activeChains.map(sc => [sc.chainId, http(sc.url)]))
 
 export const wagmiConfig = createConfig({
   chains: configuredChains as unknown as [wagmiChains.Chain, ...wagmiChains.Chain[]],
